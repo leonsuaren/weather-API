dd7c01da7795b83dd7840ca411f8187c46ec4cd2
Leon Suarez <leonsua@gmail.com>
2022-05-10 19:45:31 -0400

    finished fetching data from open weather

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 const App = () => {
-  const { useEffect, useState } = React;
+  const { useState } = React;
+  const [city, setCity] = useState('');
+  const [temp, setTemp] = useState(0);
+  const [icon, setIcon] = useState('');
+  
+  function tempToF(kelvin) {
+    return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
+  }
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios('https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=89e5d770eac3d99dee8786f4791aa5fb')
-      console.log(result);
-    }
-    fetchData();
-  }, []);
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    const result = await axios(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=89e5d770eac3d99dee8786f4791aa5fb`)
+    setTemp(tempToF(result.data.main.temp));
+    setIcon(result.data.weather[0].icon);
+    console.log(result.data);
+  }
 
   return (
-    <div className='container page-layout'>
-      <h1>hola</h1>
+    <div className='container page-layout' onSubmit={handleOnSubmit}>
+      <h1>Weather by City</h1>
+      <form>
+        <label>
+          <input className="form-control" name='city' placeholder='City' value={city} onChange={(e) => setCity(e.target.value)} />
+        </label>
+        <button className='btn btn-primary' type='submit'>Submit</button>
+      </form>
+      <div>
+        <h5>Temperature: {temp} &#8457;</h5>
+        <img 
+        src={icon === '' ? '' : `http://openweathermap.org/img/w/${icon}.png`}
+      />
+      </div>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
